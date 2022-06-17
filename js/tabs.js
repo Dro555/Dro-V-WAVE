d2bdea9881bf05c9295a3d2821b79741cfe5872c
Dro555 <ddm.su555@gmail.com>
2022-06-17 20:21:30 +0300

    caps lock failed

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,19 @@
+// Scrol a ==============================
+document.querySelectorAll(".js-scroll-link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href").substring(1);
+    const scrollTarget = document.getElementById(href);
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    window.scrollBy({
+      top: elementPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
 //Burger=================================
 document
   .querySelector(".burger-top__burger")
